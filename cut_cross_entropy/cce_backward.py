587a285ec22f091ccbd28e7c4157e7849727b139
Daniel Han <danielhanchen@gmail.com>
2025-01-06 22:57:30 -0800

    Update cce_backward.py

--- cut_cross_entropy/cce_backward.py
+++ cut_cross_entropy/cce_backward.py
@@ -253,14 +253,15 @@ def cce_backward_kernel(
     assert do.numel() in (e.size(0), 1)
     assert c.size(1) == e.size(1)
     assert lse.size(0) == e.size(0) or (valids is not None and lse.size(0) == valids.size(0))
-    assert e.dtype in (
-        torch.float16,
-        torch.bfloat16,
-    ), "Backwards requires embeddings to be bf16 or fp16"
-    assert c.dtype in (
-        torch.float16,
-        torch.bfloat16,
-    ), "Backwards requires classifier to be bf16 or fp16"
+    print(e.dtype, c.dtype)
+    # assert e.dtype in (
+    #     torch.float16,
+    #     torch.bfloat16,
+    # ), "Backwards requires embeddings to be bf16 or fp16"
+    # assert c.dtype in (
+    #     torch.float16,
+    #     torch.bfloat16,
+    # ), "Backwards requires classifier to be bf16 or fp16"
 
     do = do.contiguous()
     lse = lse.contiguous()
